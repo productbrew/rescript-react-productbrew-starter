81fb31c2be505dbbe1cad68fd684c405331c5e3d
dkmiecik <damian@productbrew.com>
2021-12-13 16:03:40 +0100

    Fixes.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders header text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Let's build something cool!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+    render(<App />)
+    const linkElement = screen.getByText(/Let's build something cool!/i)
+    expect(linkElement).toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { make as Test } from './components/Test.bs';
+import React from 'react'
+import { make as Test } from './components/Test.bs'
 
 const App: React.FC = function () {
-  return (
-    <div className="h-screen flex flex-col justify-center align-middle text-center">
-      <header>
-        <Test />
-      </header>
-    </div>
-  );
-};
+    return (
+        <div className="h-screen flex flex-col justify-center align-middle text-center">
+            <header>
+                <Test />
+            </header>
+        </div>
+    )
+}
 
-export default App;
+export default App
